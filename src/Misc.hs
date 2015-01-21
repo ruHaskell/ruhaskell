149afbca7511e457f95f3c2ab0ee1c16c7fe9c3b
Denis Shevchenko <me@dshevchenko.biz>
2015-01-21 22:41:25 +0400

    Новая статья, категория Динамика.

--- src/Misc.hs
+++ src/Misc.hs
@@ -47,6 +47,7 @@ russianNamesOfCategories = M.fromList[ ("web",      "Веб")
                                      , ("projects", "Проекты")
                                      , ("utils",    "Утилиты")
                                      , ("theory",   "Теория")
+                                     , ("dynamic",  "Динамика")
                                      , ("packages", "Пакеты")
                                      ]
 
