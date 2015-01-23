5bead9c1769371edfee4156dd43b450f5f6b651d
Antonio Nikishaev <me@lelf.lu>
2015-01-24 00:19:14 +0400

    use hscolour for haskell code highlighting

--- src/Posts.hs
+++ src/Posts.hs
@@ -17,6 +17,8 @@ import Context              (postContext)
 import Misc                 (TagsReader)
 import Control.Monad.Reader
 import Hakyll
+import qualified Text.BlogLiterately.Highlight as Highlight
+
 
 -- Дата публикации будет отражена в URL в виде подкаталогов.
 directorizeDate :: Routes
@@ -30,6 +32,14 @@ directorizeDate = customRoute (\i -> directorize $ toFilePath i)
                 minusBetweenDateAndTitle = 3
                 (date, rest) = splitAt minusBetweenDateAndTitle $ splitOn "-" path
 
+
+-- like pandocCompiler, but uses hscolour for highlighting haskell code
+pandocWithHighlighter :: Compiler (Item String)
+pandocWithHighlighter =
+    pandocCompilerWithTransform defaultHakyllReaderOptions
+                                defaultHakyllWriterOptions
+                                (Highlight.colourisePandoc Highlight.HsColourCSS True)
+
 createPosts :: TagsReader
 createPosts = do
     tagsAndAuthors <- ask
@@ -37,9 +47,9 @@ createPosts = do
     lift $ match "posts/**" $ do
         route $ directorizeDate `composeRoutes`
                 setExtension "html"
-        -- Для превращения Markdown в HTML используем pandocCompiler.
-        compile $ pandocCompiler >>= loadAndApplyTemplate "templates/post.html" (postContext tagsAndAuthors)
-                                 >>= loadAndApplyTemplate "templates/default.html" (postContext tagsAndAuthors)
-                                 >>= relativizeUrls
+        -- Для превращения Markdown в HTML используем pandocWithHighlighter
+        compile $ pandocWithHighlighter >>= loadAndApplyTemplate "templates/post.html" (postContext tagsAndAuthors)
+                                        >>= loadAndApplyTemplate "templates/default.html" (postContext tagsAndAuthors)
+                                        >>= relativizeUrls
     return ()
 
