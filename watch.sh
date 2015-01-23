468f9fbc250fa978379245150ea477afb223b8cb
Denis Redozubov <denis.redozubov@gmail.com>
2015-01-23 23:22:20 +0300

    Апдейт watch-скрипта(поддержка OS X etc)
    * поддержка OS X через fswatch(на OS X нет inotifywait)
    * кабал запускается с -v, добавляя вербозность при любых локальных настройках
    * список "наблюдаемых" файлов сужен до *.md для постов и *.hs для сорцов

--- watch.sh
+++ watch.sh
@@ -1,6 +1,24 @@
-while inotifywait -qq -e modify -r posts/ src/
-do
-    clear
-    cabal run build
-    echo "BrowserReload();" | nc localhost 4242  > /dev/null
-done
+WATCH_LIST="posts/*.md src/*.hs"
+CMD="
+clear
+cabal run build -v
+echo 'BrowserReload();' | nc localhost 4242  > /dev/null
+"
+
+case "$OSTYPE" in
+darwin*)
+  fswatch -0 -r $WATCH_LIST | while read -d "" event
+  do
+    eval "$CMD"
+  done
+  ;;
+linux*)
+  while inotifywait -qq -e modify -r $WATCH_LIST
+  do
+    eval "$CMD"
+  done
+  ;;
+*)
+  echo "Unknown OS: $OSTYPE"
+  ;;
+esac
