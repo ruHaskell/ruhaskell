b04ff67ddf48dde9c0a8eb053fb0201c048f517a
Denis Shevchenko <me@dshevchenko.biz>
2015-02-08 19:58:09 +0400

    Удаление span из имени.

--- src/Tags.hs
+++ src/Tags.hs
@@ -52,45 +52,45 @@ buildPostsAuthors = buildTagsWith getNameOfAuthor "posts/**" $ fromCapture "auth
 -- Функция отрисовывает тег-ссылку вместе со значком, отражающим количество публикаций,
 -- соответствующих данному тегу. Например, количество статей данного автора.
 -- За основу взяты исходники Hakyll.
-createTagLinkWithBadge :: Double 
-                       -> Double 
+createTagLinkWithBadge :: Double
+                       -> Double
                        -> String
                        -> String
-                       -> Int 
+                       -> Int
                        -> Int
                        -> Int
                        -> String
 createTagLinkWithBadge = createGenericTagLinkWithBadge id
 
 -- Отрисовываем тег для категории, с заменой родного английского названия русским аналогом.
-createRussianTagLinkWithBadge :: Double 
-                              -> Double 
+createRussianTagLinkWithBadge :: Double
+                              -> Double
                               -> String
                               -> String
-                              -> Int 
+                              -> Int
                               -> Int
                               -> Int
                               -> String
 createRussianTagLinkWithBadge = createGenericTagLinkWithBadge getRussianNameOfCategory
 
 createGenericTagLinkWithBadge :: (String -> String)
-                              -> Double 
-                              -> Double 
+                              -> Double
+                              -> Double
                               -> String
                               -> String
-                              -> Int 
+                              -> Int
                               -> Int
                               -> Int
                               -> String
 {-# INLINE createGenericTagLinkWithBadge #-}
 createGenericTagLinkWithBadge convert
-                              smallestFontSizeInPercent 
+                              smallestFontSizeInPercent
                               biggestFontSizeInPercent
                               tag
                               url
                               count
                               min'
-                              max' = 
+                              max' =
     let diff     = 1 + fromIntegral max' - fromIntegral min'
         relative = (fromIntegral count - fromIntegral min') / diff
         size     = floor $ smallestFontSizeInPercent + relative * (biggestFontSizeInPercent - smallestFontSizeInPercent) :: Int
@@ -112,9 +112,9 @@ renderTagCloudWithBadges :: Double
 renderTagCloudWithBadges smallestFontSizeInPercent
                          biggestFontSizeInPercent
                          specificTags
-                         thisIsCategoriesCloud =    
-    let tagLinkRenderer = if thisIsCategoriesCloud 
-                          then createRussianTagLinkWithBadge 
+                         thisIsCategoriesCloud =
+    let tagLinkRenderer = if thisIsCategoriesCloud
+                          then createRussianTagLinkWithBadge
                           else createTagLinkWithBadge
     in
     renderTagCloudWith tagLinkRenderer
@@ -125,33 +125,33 @@ renderTagCloudWithBadges smallestFontSizeInPercent
     where concatenateLinksWithSpaces = intercalate ", "
 
 -- Вспомогательная функция, формирующая страницу с облаком определённых тегов.
-createPageWithTagsCloud :: Tags 
+createPageWithTagsCloud :: Tags
                         -> Identifier
                         -> Double
                         -> Double
-                        -> String 
+                        -> String
                         -> String
                         -> Identifier
                         -> Rules ()
-createPageWithTagsCloud specificTags 
-                        pageWithSpecificTags 
-                        smallestFontSizeInPercent 
-                        biggestFontSizeInPercent 
-                        pageTitle 
-                        cloudName 
-                        specificTemplate = 
+createPageWithTagsCloud specificTags
+                        pageWithSpecificTags
+                        smallestFontSizeInPercent
+                        biggestFontSizeInPercent
+                        pageTitle
+                        cloudName
+                        specificTemplate =
     create [pageWithSpecificTags] $ do
         route idRoute
         compile $ do
-            let renderedCloud _ = renderTagCloudWithBadges smallestFontSizeInPercent 
+            let renderedCloud _ = renderTagCloudWithBadges smallestFontSizeInPercent
                                                            biggestFontSizeInPercent
                                                            specificTags
                                                            ("Категории" `isInfixOf` pageTitle)
-                tagsContext = mconcat [ constField "title" pageTitle 
+                tagsContext = mconcat [ constField "title" pageTitle
                                       , field cloudName renderedCloud
                                       , defaultContext
                                       ]
-            
+
             makeItem "" >>= loadAndApplyTemplate specificTemplate tagsContext
                         >>= loadAndApplyTemplate "templates/default.html" tagsContext
                         >>= relativizeUrls
@@ -161,9 +161,9 @@ createPageWithAllTags :: TagsReader
 createPageWithAllTags = do
     tagsAndAuthors <- ask
     lift $ createPageWithTagsCloud (head tagsAndAuthors)
-                                   "tags.html" 
-                                   110 
-                                   220 
+                                   "tags.html"
+                                   110
+                                   220
                                    "Темы публикаций"
                                    "tagsCloud"
                                    "templates/tags.html"
@@ -173,12 +173,12 @@ createPageWithAllTags = do
 createPageWithAllCategories :: TagsReader
 createPageWithAllCategories = do
     tagsAndAuthors <- ask
-    lift $ createPageWithTagsCloud (tagsAndAuthors !! 1) 
-                                   "categories.html" 
-                                   110 
-                                   220 
-                                   "Категории публикаций" 
-                                   "categoriesCloud" 
+    lift $ createPageWithTagsCloud (tagsAndAuthors !! 1)
+                                   "categories.html"
+                                   110
+                                   220
+                                   "Категории публикаций"
+                                   "categoriesCloud"
                                    "templates/categories.html"
     return ()
 
@@ -186,23 +186,23 @@ createPageWithAllCategories = do
 createPageWithAllAuthors :: TagsReader
 createPageWithAllAuthors = do
     tagsAndAuthors <- ask
-    lift $ createPageWithTagsCloud (tagsAndAuthors !! 2) 
-                                   "authors.html" 
-                                   110 
-                                   220 
-                                   "Наши авторы" 
-                                   "authorsCloud" 
+    lift $ createPageWithTagsCloud (tagsAndAuthors !! 2)
+                                   "authors.html"
+                                   110
+                                   220
+                                   "Наши авторы"
+                                   "authorsCloud"
                                    "templates/authors.html"
     return ()
 
-convertSpecificTagsToLinks :: TagsAndAuthors 
-                           -> Tags 
-                           -> String 
+convertSpecificTagsToLinks :: TagsAndAuthors
+                           -> Tags
+                           -> String
                            -> Rules ()
-convertSpecificTagsToLinks tagsAndAuthors specificTags aTitle = 
+convertSpecificTagsToLinks tagsAndAuthors specificTags aTitle =
     tagsRules specificTags $ \tag pattern -> do
         let nameOfTag = if "категории" `isInfixOf` aTitle then getRussianNameOfCategory tag else tag
-            title = renderHtml $ H.preEscapedToHtml $ aTitle ++ " <span class=\"tag-in-title\">" ++ nameOfTag ++ "</span>"
+            title = renderHtml $ H.preEscapedToHtml $ aTitle ++ " " ++ nameOfTag
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll pattern
@@ -211,7 +211,7 @@ convertSpecificTagsToLinks tagsAndAuthors specificTags aTitle =
                                              , defaultContext
                                              ]
 
-            makeItem "" >>= loadAndApplyTemplate "templates/posts.html" taggedPostsContext 
+            makeItem "" >>= loadAndApplyTemplate "templates/posts.html" taggedPostsContext
                         >>= loadAndApplyTemplate "templates/default.html" taggedPostsContext
                         >>= relativizeUrls
 
@@ -219,7 +219,7 @@ convertSpecificTagsToLinks tagsAndAuthors specificTags aTitle =
 convertTagsToLinks :: TagsReader
 convertTagsToLinks = do
     tagsAndAuthors <- ask
-    lift $ convertSpecificTagsToLinks tagsAndAuthors 
+    lift $ convertSpecificTagsToLinks tagsAndAuthors
                                       (head tagsAndAuthors)
                                       "Все статьи по теме"
     return ()
@@ -228,7 +228,7 @@ convertTagsToLinks = do
 convertCategoriesToLinks :: TagsReader
 convertCategoriesToLinks = do
     tagsAndAuthors <- ask
-    lift $ convertSpecificTagsToLinks tagsAndAuthors 
+    lift $ convertSpecificTagsToLinks tagsAndAuthors
                                       (tagsAndAuthors !! 1)
                                       "Все статьи в категории"
     return ()
@@ -237,7 +237,7 @@ convertCategoriesToLinks = do
 convertAuthorsToLinks :: TagsReader
 convertAuthorsToLinks = do
     tagsAndAuthors <- ask
-    lift $ convertSpecificTagsToLinks tagsAndAuthors 
+    lift $ convertSpecificTagsToLinks tagsAndAuthors
                                       (tagsAndAuthors !! 2)
                                       "Все статьи автора"
     return ()
