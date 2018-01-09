24f449008a87ce121e65456996841df01816d496
Yuriy Syrovetskiy <cblp@cblp.su>
2018-01-10 01:04:28 +0300

    Fix mathjax link

--- src/Markup/Head.hs
+++ src/Markup/Head.hs
@@ -5,49 +5,50 @@ module Markup.Head (
     commonHead
 ) where
 
-import           Css.Own                        ( ownCss )
-import           Misc                           ( aHost )
+import           Css.Own (ownCss)
+import           Misc (aHost)
 
-import           Prelude                        hiding ( div, span, head )
-import           Text.Blaze.Html5
-import qualified Text.Blaze.Html5.Attributes    as A
-import           Data.String.QQ
+import           Prelude hiding (div, head, span)
+
 import           Data.Monoid
+import           Data.String.QQ
+import           Text.Blaze.Html5
+import qualified Text.Blaze.Html5.Attributes as A
 
 commonHead :: Html
 commonHead = head $ do
     title "$title$"
-    
+
     meta ! A.charset "utf-8"
 
     -- Для мобильных экранов.
     meta ! A.name "viewport"
          ! A.content "width=device-width, initial-scale=1.0"
-    
+
     meta ! A.name "description"
          ! A.content ""
-    
+
     meta ! A.name "author"
          ! A.content ""
 
     link ! A.rel "icon"
          ! A.type_ "image/png"
          ! A.href (toValue $ imgRoot <> "favicon.ico")
-    
+
     cssLink "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
     jsLink  "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"
     cssLink "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"
     jsLink  "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"
-    jsLink  "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+    jsLink  "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 
-    style $ toHtml ownCss 
-    
+    style $ toHtml ownCss
+
     -- Аналитика.
     script $ toHtml googleAnalytics
 
     script $ toHtml ("window.___gcfg = { lang: 'ru' };" :: String)
     script ! A.src "https://apis.google.com/js/platform.js" $ "" -- async defer
-    
+
     -- RSS feed.
     link ! A.rel "alternate"
          ! A.type_ "application/rss+xml"
@@ -56,7 +57,7 @@ commonHead = head $ do
   where
     cssLink :: String -> Html
     cssLink url = link ! A.rel "stylesheet" ! A.href (toValue url)
-    
+
     jsLink :: String -> Html
     jsLink url = script ! A.src (toValue url) $ ""
 
