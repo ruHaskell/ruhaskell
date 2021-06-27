d054ccda7b53f9abdf8e20094413c7128b6a455d
Yuriy Syrovetskiy <root@localhost>
2021-06-27 16:15:02 +0200

    Update resolver from 14.0 to 16.31 (#115)
    * replacement of deprecated functions from hakyll with new ones
    * update resolver from 14.0 to 16.31
    * add support of Compiler-monad

--- watch.sh
+++
@@ -1,6 +0,0 @@
-while inotifywait -qq -e modify -r posts/ src/
-do
-    clear
-    cabal run build
-    echo "BrowserReload();" | nc localhost 4242  > /dev/null
-done
--- withHakyll_build.sh
+++
@@ -1,7 +0,0 @@
-mkdir dist
-set -e
-ghc -threaded -o dist/pkg Setup.hs
-dist/pkg configure
-dist/pkg build
-dist/build/ruHaskell/ruHaskell build
-dist/build/ruHaskell/ruHaskell check
